480f41cbb0799de4ebc830635b1d4826c6aa5faa
Jan-Peter Wittig <jan-peter.wittig@bauermedia.com>
2025-05-13 12:43:40 +0200

    Korrigiere kleinere Tippfehler in Biography.vue
    Es wurden zwei kleinere Tippfehler in der Biografie-Komponente korrigiert. Diese Änderungen verbessern die Lesbarkeit und Präzision des Textes.

--- portfolio-app/resources/js/components/Portfolio/Biography.vue
+++ portfolio-app/resources/js/components/Portfolio/Biography.vue
@@ -5,7 +5,7 @@
             Ich bin ein leidenschaftlicher Software- und Web-Entwickler mit Erfahrung in modernen Web-Technologien.
             Mein Schwerpunkt liegt auf der Entwicklung von Backends mithilfe von PHP und dem Symfony Framework
             oder dem Laravel Framework.<br>
-            Für die Entwicklung von passenden Frontends nutze ich Vue.JS.
+            Für die Entwicklung von passenden Frontends nutze ich Vue.JS.<br>
             In meiner bisherigen Laufbahn bei der Bauer Media Group bin ich primär als System-Administrator tätig.
             Im Rahmen meiner Tätigkeit habe ich zusätzlich die Aufgabe übernommen ein Legacy-System in PHP neu zu entwickeln.
             Dadurch war es mir möglich viel über moderne Softwareentwicklung zu lernen. Konkret habe ich mir durch dieses Projekt
@@ -14,7 +14,7 @@
             meine Tätigkeit als Sys-Admin haben sich auch in Bereich Softwareentwicklung bezahlt gemacht.
 
             Neben meinen beruflichen Tätigkeiten entwickle ich als Hobby Videospiele in der Unreal Engine
-            und nutze hierfür C++ um die Möglichkeiten der Engine an Anforderungen meiner Projekte anzupassen.
+            und nutze hierfür C++ um die Möglichkeiten der Engine an die Anforderungen meiner Projekte anzupassen.
         </p>
     </div>
 </template>
